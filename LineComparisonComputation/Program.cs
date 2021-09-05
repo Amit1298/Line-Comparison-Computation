7f4b2bc2f7cdb20f4439ef87c65a89429d160973
Amit <amitkr24031995@gmail.com>
2021-09-05 08:46:57 +0530

    Added Welcome Message

---
+++ LineComparisonComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparisonComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation");
+            Console.ReadLine();
+        }
+    }
+}
